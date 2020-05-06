a1ceef08f96284c7c8f3a7e4fd1b905ce0c4fd45
Odranium <pelletierdorian@gmail.com>
2020-05-06 23:17:49 +0200

    .

---
+++ build/precache-manifest.197d5cc2d2dc110e2d5b51d37b58a9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b846752dbba96bbed8411bdacf0aba26",
+    "url": "/MyWebTrainer-ReactJS/index.html"
+  },
+  {
+    "revision": "ef8f9ff85856787a99b2",
+    "url": "/MyWebTrainer-ReactJS/static/css/main.6ea6177b.chunk.css"
+  },
+  {
+    "revision": "6019b54aa2e39cd6ec9f",
+    "url": "/MyWebTrainer-ReactJS/static/js/2.3c4c049d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyWebTrainer-ReactJS/static/js/2.3c4c049d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8f9ff85856787a99b2",
+    "url": "/MyWebTrainer-ReactJS/static/js/main.fa8162a6.chunk.js"
+  },
+  {
+    "revision": "fea4ed111159c0d1821e",
+    "url": "/MyWebTrainer-ReactJS/static/js/runtime-main.564796ad.js"
+  }
+]);
